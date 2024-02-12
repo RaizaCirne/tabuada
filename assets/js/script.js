d97b10085aaf164b3e11b1069aa3eeb482bca155
Raiza Cirne <raizacirne@gmail.com>
2024-02-12 13:24:33 -0300

    Feat: add style

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-
-let botao = document.getElementById('button-calcular'); 
-console.log(botao);
-
-botao.addEventListener('click', function() {
-  let valores = document.getElementById('number-input');
-  let guardar = valores.value;
-  console.log(guardar);
-})
